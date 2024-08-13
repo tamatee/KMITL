71d46b8daf034d2062f6f151b8bba1cded79633e
tamatee <teeracharoenkitisap@gmail.com>
2024-08-13 10:32:48 +0700

    [tamate -> tamate] update dir

--- Year3/ComSys/lab0/queue.c
+++ Year3/ComSys/lab0/queue.c
@@ -58,16 +58,13 @@ bool q_insert_head(queue_t *q, char *s)
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
     if (q == NULL)
-    {
         return false;
-    }
     
     newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
+    
     if (newh == NULL)
-    {
         return false;
-    }
 
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
